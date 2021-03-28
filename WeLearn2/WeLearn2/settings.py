72e2b5586b513598f7c415ec77a7fa51c2c03712
Vicky-Raj <vickylab2000@gmail.com>
2021-03-28 21:27:50 +0530

    v2

--- WeLearn2/WeLearn2/settings.py
+++ WeLearn2/WeLearn2/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'home.apps.HomeConfig',
-    'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'colorfield'
+    'colorfield',
+    'home',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'WeLearn2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'WeLearn',
+#         'USER': 'postgres',
+#         'PASSWORD': '****',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'WeLearn',
-        'USER': 'postgres',
-        'PASSWORD': '****',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
--- WeLearn2/home/admin.py
+++ WeLearn2/home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Pack)
+admin.site.register(Link)
+admin.site.register(Tag)
+admin.site.register(Notification)
+admin.site.register(Comment)
+admin.site.register(Reply)
+
+
+
+
+
